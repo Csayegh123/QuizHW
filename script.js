4b63ffa253a96a182b0c04a428b40572adab4fc0
Christopher <sayegh.topher@gmail.com>
2021-04-09 15:07:10 -0500

    Quiz HW update

---
+++ script.js
@@ -0,0 +1,216 @@
+
+const startButton = document.getElementById('start-btn')
+
+const nextButton = document.getElementById('next-btn')
+
+const questionContainerElement = document.getElementById('question-container')
+
+const questionElement = document.getElementById('question')
+
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+
+nextButton.addEventListener('click', () => {
+
+  currentQuestionIndex++
+
+  setNextQuestion()
+
+})
+
+function startGame() {
+
+  startButton.classList.add('hide')
+
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+
+  currentQuestionIndex = 0
+
+  questionContainerElement.classList.remove('hide')
+
+  setNextQuestion()
+
+}
+
+function setNextQuestion() {
+
+  resetState()
+
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(question) {
+
+  questionElement.innerText = question.question
+
+  question.answers.forEach(answer => {
+
+    const button = document.createElement('button')
+
+    button.innerText = answer.text
+
+    button.classList.add('btn')
+
+    if (answer.correct) {
+
+      button.dataset.correct = answer.correct
+
+    }
+
+    button.addEventListener('click', selectAnswer)
+
+    answerButtonsElement.appendChild(button)
+
+  })
+
+}
+
+function resetState() {
+
+  clearStatusClass(document.body)
+
+  nextButton.classList.add('hide')
+
+  while (answerButtonsElement.firstChild) {
+
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+
+  }
+
+}
+
+function selectAnswer(e) {
+
+  const selectedButton = e.target
+
+  const correct = selectedButton.dataset.correct
+
+  setStatusClass(document.body, correct)
+
+  Array.from(answerButtonsElement.children).forEach(button => {
+
+    setStatusClass(button, button.dataset.correct)
+
+  })
+
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+
+    nextButton.classList.remove('hide')
+
+  } else {
+
+    startButton.innerText = 'Restart'
+
+    startButton.classList.remove('hide')
+
+  }
+
+}
+
+function setStatusClass(element, correct) {
+
+  clearStatusClass(element)
+
+  if (correct) {
+
+    element.classList.add('correct')
+
+  } else {
+
+    element.classList.add('wrong')
+
+  }
+
+}
+
+function clearStatusClass(element) {
+
+  element.classList.remove('correct')
+
+  element.classList.remove('wrong')
+
+}
+
+const questions = [
+
+  {
+
+    question: 'Why do we call dried grapes raisins, but call dried bananas "dried bananas"?',
+
+    answers: [
+
+      { text: 'Bananas are not fancy enough for a nick name of its dried state', correct: false },
+
+      { text: 'this just blew my mind', correct: true }
+
+    ]
+
+  },
+
+  {
+
+    question: 'Who is #1?',
+
+    answers: [
+
+      { text: 'I am', correct: true },
+
+      { text: 'My dog', correct: true },
+
+      { text: 'My cat', correct: true },
+
+      { text: 'Will Ferrel', correct: true }
+
+    ]
+
+  },
+
+  {
+
+    question: 'What is the best music to listen to while in a class?',
+
+    answers: [
+
+      { text: 'Metal', correct: false },
+
+      { text: 'None, pay attention to the class', correct: true },
+
+      { text: 'Pop', correct: false },
+
+      { text: 'Opera', correct: false }
+
+    ]
+
+  },
+
+  {
+
+    question: 'What is a 10X2?',
+
+    answers: [
+
+      { text: '12', correct: false },
+
+      { text: '20', correct: true }
+
+    ]
+
+  }
+
+]
+
+var count = 25;
+var interval = setInterval(function.(){
+  document.getElementById('count').innerHTML=count;
+  count--;
+  if (count === 0){
+    clearInterval(interval);
+    document.getElementById('count').innerHTML='Done';
+    // or...
+    alert("You're out of time!");
+  }
+}, 1000);
